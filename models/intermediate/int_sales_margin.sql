955fe97272bb90ca8c0a0b456d945452e8614334
Selma Chachia <chachia.selma@gmail.com>
2024-05-01 06:27:13 +0000

    ok

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -9,5 +9,5 @@ select
     ROUND(s.quantity*CAST(purchase_price as FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(purchase_price as FLOAT64),2) AS margin,
 from {{ ref('stg_raw__sales')}} s
-LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product')}} p
+LEFT JOIN {{ ref('stg_raw_product')}} p
     USING(products_id)
