ff7f1f259b462e6075e2feccd2752172a8190226
Selma Chachia <chachia.selma@gmail.com>
2024-05-01 09:44:25 +0000

    ok

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,14 +1,15 @@
-select
-o.orders_id
-,o.date_date,ROUND(o.margin + s.shipping_fee - (s.log.cost + s.ship_cost),2) AS operational_margin
-,o.quantity
-,o.revenue
-,o.purchase_cost
-,o.margin
-,s.shipping_fee
-,s.logcost
-,s.ship_cost
-FROM {{ref("int_sales_margin")}} o
-LEFT JOIN {{ref("stg_raw_ship")}} s
-    USING(orders_id)
-ORDER BY orders_id DESC
+SELECT
+  o.orders_id
+  ,o.date_date
+  ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw_ship")}} s 
+  USING(orders_id)
+ORDER BY orders_id desc
