3b93bfa10522bfae420b979593eb723aa8e9d505
Selma Chachia <chachia.selma@gmail.com>
2024-05-01 14:18:29 +0000

    ok

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,8 @@
+SELECT 
+date_date
+,operational_margin - ads_cost as ads_margin
+,average_basket as average_basket
+,operational_margin as operational_margin
+FROM {{ref("int.campaigns_day")}} 
+LEFT JOIN {{ref("finance_days")}} 
+USING(date_date)
